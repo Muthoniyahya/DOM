6c338f670445069739dd3f8fa3e8cf8453c90ba4
student <Muthoniyahya@gmail.com>
2021-06-16 20:16:17 +0300

    first commit

--- cat.js
+++ cat.js
@@ -10,7 +10,10 @@ var lisaLab = {
         }());
     }
 };
+    lisaLab.func()
 
-lisaLab.func();
+// lisaLab.func(); the output will be all the names because its a global scope and can be able to access the outer and inner scope
+// func:function can be able to access the outer scope and itself also meaning the output will be bar and the names printed inside console.log
+// function()can only access itself and not the other scopes
 
 
